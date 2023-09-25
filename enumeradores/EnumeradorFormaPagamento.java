8782b34b90245b26f0712f08971a818f5e70ef9a
Everton Silva <48794204+EvertonFSilva@users.noreply.github.com>
2023-09-25 02:49:35 -0300

    Delete enumeradores directory

--- enumeradores/EnumeradorFormaPagamento.java
+++
@@ -1,6 +0,0 @@
-package enumeradores;
-
-public enum EnumeradorFormaPagamento {
-	DINHEIRO, CARTAO_CREDITO, CARTAO_DEBITO, PIX, BOLETO_BANCARIO
-}
-
--- enumeradores/EnumeradorPeriodo.java
+++
@@ -1,7 +0,0 @@
-package enumeradores;
-
-public enum EnumeradorPeriodo {
-	DIARIO, MENSAL, TRIMESTRAL, SEMESTRAL, ANUAL
-}
-
-
